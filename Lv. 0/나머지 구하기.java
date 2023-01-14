a736df06b2913c836a619e9e82d2899a3e35d42c
beco <galashow7@helixtech.co.kr>
2023-01-15 00:12:40 +0900

    [Lv. 0]나머지 구하기

--- Lv. 0/나머지 구하기.java
+++ Lv. 0/나머지 구하기.java
@@ -1,13 +1,23 @@
 /**
- * 문제 설명
- * 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+ *문제 설명
+ * 중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
  *
  * 제한사항
- * 0 < num1 ≤ 100
- * 0 < num2 ≤ 100
+ * array의 길이는 홀수입니다.
+ * 0 < array의 길이 < 100
+ * -1,000 < array의 원소 < 1,000
  */
 class Solution {
-    public int solution(int num1, int num2) {
-        return num1%num2;
+    public int solution(int[] array) {
+        for(int i=0; i < array.length; i++) {
+            for(int j = i+1; j < array.length; j++) {
+                if(array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array[array.length/2];
     }
 }
